94f9e0d748d959f54eee60d1b533e0fc12b38bc8
boradol2 <boradol0902@gmail.com>
2024-11-21 22:30:41 +0900

    feat(InputView): 자동차 이름 입력받을 문구 추가

--- src/main/kotlin/racingcar/GameApplication.kt
+++ src/main/kotlin/racingcar/GameApplication.kt
@@ -2,10 +2,14 @@ package racingcar
 
 import racingcar.strategy.RandomMoveStrategy
 import racingcar.ui.printAllResults
+import racingcar.ui.readCarNames
 import racingcar.ui.readNumberOfCars
 import racingcar.ui.readTries
 
 fun main() {
+    val carNames = readCarNames()
+    println("carNames = $carNames")
+
     val carCount = readNumberOfCars()
     val round = readTries()
     val raceResults = CarRacingGame(carCount, round).play(RandomMoveStrategy())
--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -5,6 +5,11 @@ fun readNumberOfCars(): Int {
     return readln().toInt()
 }
 
+fun readCarNames(): String {
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    return readln()
+}
+
 fun readTries(): Int {
     println("시도할 횟수는 몇 회인가요?")
     return readln().toInt()
