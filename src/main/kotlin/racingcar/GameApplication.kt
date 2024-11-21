1bc56410e252891ed718595a8c42d5b069b12d0a
boradol2 <boradol0902@gmail.com>
2024-11-22 03:03:32 +0900

    feat(ResultView): 전진하는 자동차를 출력할 때 자동차 이름 같이 출력 하기

--- src/main/kotlin/racingcar/GameApplication.kt
+++ src/main/kotlin/racingcar/GameApplication.kt
@@ -2,12 +2,18 @@ package racingcar
 
 import racingcar.strategy.RandomMoveStrategy
 import racingcar.ui.printAllResults
+import racingcar.ui.printWinners
 import racingcar.ui.readCarNames
 import racingcar.ui.readTries
 
 fun main() {
     val carNames = readCarNames()
     val round = readTries()
-    val raceResults = CarRacingGame.create(carNames, round).play(RandomMoveStrategy())
+
+    val game = CarRacingGame.create(carNames, round)
+    val raceResults = game.play(RandomMoveStrategy())
+    val winners = game.findWinners(raceResults)
+
     printAllResults(raceResults)
+    printWinners(winners)
 }
--- src/main/kotlin/racingcar/ui/ResultView.kt
+++ src/main/kotlin/racingcar/ui/ResultView.kt
@@ -1,11 +1,16 @@
 package racingcar.ui
 
+import racingcar.domain.Car
 import racingcar.domain.Cars
 
 private const val DASH_SYMBOL = "-"
 
+fun printWinners(winners: List<Car>) {
+    println("${winners.joinToString(", ") { it.name.value }}가 최종 우승했습니다.")
+}
+
 fun printAllResults(raceResult: List<Cars>) {
-    println("\n실행결과")
+    println("\n실행결과\n")
     raceResult.forEach { round ->
         printRoundResult(round)
         println()
@@ -13,7 +18,7 @@ fun printAllResults(raceResult: List<Cars>) {
 }
 
 private fun printRoundResult(round: Cars) {
-    round.getPositions().forEach { position ->
-        println(DASH_SYMBOL.repeat(position))
+    round.currentCars().forEach { car ->
+        println("${car.name.value} : ${DASH_SYMBOL.repeat(car.position.value)}")
     }
 }
