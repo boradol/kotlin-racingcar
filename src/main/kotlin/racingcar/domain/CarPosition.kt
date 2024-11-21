06da37ce90c94ba747aeb48fadbcba7307eee594
boradol2 <boradol0902@gmail.com>
2024-11-22 00:17:11 +0900

    refactor(CarPosition): CarPosition 예외 적용

--- src/main/kotlin/racingcar/domain/CarPosition.kt
+++ src/main/kotlin/racingcar/domain/CarPosition.kt
@@ -3,6 +3,10 @@ package racingcar.domain
 private const val DEFAULT_POSITION = 0
 
 data class CarPosition(val value: Int = DEFAULT_POSITION) {
+    init {
+        require(value >= DEFAULT_POSITION) { "위치는 음수일 수 없습니다." }
+    }
+
     fun move(): CarPosition {
         return copy(value = value + 1)
     }
